dae5d472f68d6071f6a2176c5fe85c52bc4b70d2
Sam Gallo <saangall@ucsc.edu>
2026-04-14 15:11:03 -0700

    Stuff yeah

---
+++ RPNEvaluator/RPNEvaluator.Tests/RPNEvaluatorTests.cs
@@ -0,0 +1,76 @@
+﻿using Xunit;
+using RPNEvaluator;
+
+namespace RPNEvaluator.Tests;
+
+public class RPNEValuatorTests
+{
+    // [Fact] = a single, fixed test
+    [Fact]
+    public void TestOnePlusOne()
+    {
+        var result = RPNEvaluator.Evaluate("1 1 +", null);
+        Assert.Equal(2, result);
+    }
+
+    // [Theory] = multiple tests with parameters
+    [Theory]
+    [InlineData("0 1 *", 0)]
+    [InlineData("1 1 -", 0)]
+    [InlineData("1 2 /", 0)]
+    public void TestFormula(string formula, int expected)
+    {
+        var result = RPNEvaluator.Evaluate(formula, null);
+        Assert.Equal(expected, result);
+    }
+    [Fact]
+    public void WithDict()
+    {
+        var result = RPNEvaluator.Evaluate("x y +", new Dictionary<string, int> { { "x", 1}, { "y", 1} });
+        Assert.Equal(2, result);
+    }
+
+
+    //Evaluate with float with float input
+    [Fact]
+    public void TestOnePlusOneFloat()
+    {
+        var result = RPNEvaluator.Evaluatef("1 1 +", null);
+        Assert.Equal(2.0, result);
+    }
+
+    [Fact]
+    public void DivisionFloat()
+    {
+        var result = RPNEvaluator.Evaluatef("1 2 /", null);
+        Assert.Equal(0.5, result);
+    }
+
+    [Theory]
+    [InlineData("4 3 %", 1.0)]
+    //[InlineData("9 12 *", 108.0)]
+    //[InlineData("25 5 /", 5.0)]
+
+    public void TestFormulaFloatVariant(string formula, float expected)
+    {
+        var result = RPNEvaluator.Evaluatef(formula, null);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void WithDictFloat()
+    {
+        var result = RPNEvaluator.Evaluatef("x y /", new Dictionary<string, float> { { "x", 25 }, { "y", 5 } });
+        Assert.Equal(5.0, result);
+    }
+
+    [Fact]
+    public void WithDiv()
+    {
+        var result = RPNEvaluator.Evaluatef("x y *", new Dictionary<string, float> { { "x", 9 }, { "y", 12 } });
+        Assert.Equal(108.0, result);
+    }
+
+    //Float with int input for the dict
+}
+
---
+++ RPNEvaluator/RPNEvaluator/RPNEvaluator.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RPNEvaluator
+{
+    public class RPNEvaluator
+    {
+        public static int Evaluate(string list, Dictionary<string, int>? dict)
+        {
+            
+            if(list == null)
+            {
+                return 0;
+            }
+
+            var stack = new Stack<int>();
+            int i;
+            int first;
+            int second;
+            int result;
+
+            for(i = 0; i < list.Length; i += 1)
+            {
+                if (!char.IsDigit(list[i]))
+                {
+                    if (list[i] == '+')
+                    {
+                        second = stack.Pop();
+                        first = stack.Pop();
+                        result = first + second;
+
+                        stack.Push(result);
+                    }
+
+                    else if (list[i] == '-')
+                    {
+                        second = stack.Pop();
+                        first = stack.Pop();
+                        result = first - second;
+
+                        stack.Push(result);
+                    }
+
+                    else if (list[i] == '*')
+                    {
+                        second = stack.Pop();
+                        first = stack.Pop();
+                        result = first * second;
+
+                        stack.Push(result);
+                    }
+
+                    else if (list[i] == '/')
+                    {
+                        second = stack.Pop();
+                        first = stack.Pop();
+                        result = first / second;
+
+                        stack.Push(result);
+                    }
+
+                    else if (list[i] == '%')
+                    {
+                        second = stack.Pop();
+                        first = stack.Pop();
+                        result = first % second;
+
+                        stack.Push(result);
+                    }
+
+                    else
+                    {
+                        if (dict != null)
+                        {
+                            foreach (var (key, value) in dict)
+                            {
+                                if (key == list[i].ToString())
+                                {
+                                    stack.Push(value);
+                                }
+                            }
+                        }
+                     }
+
+                }
+                else
+                {
+                    result = list[i] - '0';
+                    stack.Push(result);
+                }
+            }
+            return stack.Pop();
+        }
+     public static float Evaluatef(string list, Dictionary<string, float>? dict)
+        {
+            if (list == null)
+            {
+                return 0;
+            }
+
+            var stack = new Stack<float>();
+            int i;
+            float first;
+            float second;
+            float result;
+
+            for (i = 0; i < list.Length; i += 1)
+            {
+                if (!char.IsDigit(list[i]))
+                {
+                    if (list[i] == '+')
+                    {
+                        second = stack.Pop();
+                        first = stack.Pop();
+                        result = first + second;
+
+                        stack.Push(result);
+                    }
+
+                    else if (list[i] == '-')
+                    {
+                        second = stack.Pop();
+                        first = stack.Pop();
+                        result = first - second;
+
+                        stack.Push(result);
+                    }
+
+                    else if (list[i] == '*')
+                    {
+                        second = stack.Pop();
+                        first = stack.Pop();
+                        result = first * second;
+
+                        stack.Push(result);
+                    }
+
+                    else if (list[i] == '/')
+                    {
+                        second = stack.Pop();
+                        first = stack.Pop();
+                        result = first / second;
+
+                        stack.Push(result);
+                    }
+
+                    else if (list[i] == '%')
+                    {
+                        second = stack.Pop();
+                        first = stack.Pop();
+                        result = first % second;
+
+                        stack.Push(result);
+                    }
+
+                    else
+                    {
+                        if (dict != null)
+                        {
+                            foreach (var (key, value) in dict)
+                            {
+                                if (key == list[i].ToString())
+                                {
+                                    stack.Push(value);
+                                }
+                            }
+                        }
+                    }
+
+                }
+                else
+                {
+                    result = list[i] - '0';
+                    stack.Push(result);
+                }
+            }
+            return stack.Pop();
+        }
+    }
+}
